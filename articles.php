ef7e9cf657c36c6757904b36549006c100bf53aa
Leo Quelis <leo.quelis@etu.unice.fr>
2022-12-26 22:15:33 +0100

    Amélioration page articles

--- articles.php
+++ articles.php
@@ -1,103 +1,137 @@
 <?php
-    session_start();
-    $GLOBALS["page"] = "articles.php";
-    require_once('index.php');
-    require_once('database/Database.php');
+session_start();
+$GLOBALS["page"] = "articles.php";
+require_once('index.php');
+require_once('database/Database.php');
+
 ?>
 
 <!DOCTYPE html>
 <html LANG="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/responsive.css">
-    <title>Magasin Virtuel</title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/responsive.css">
+  <title>Magasin Virtuel</title>
 </head>
+
 <body>
-    <main>
-        <h1>Articles</h1>
-        <div class = "article">
-        <table>
-            <tbody>
-                <?php
-                    ConnexionDB::getInstance();
-                    $sql = "SELECT id FROM produit";
-                    $result = ConnexionDB::getInstance()->querySelect($sql);
-                    $nbProduct = count($result);
-            
-                    for ($i = 1; $i <= $nbProduct; $i++) {
-                        $nom = "SELECT nom FROM produit WHERE id = $i";
-                        $result = ConnexionDB::getInstance()->querySelect($nom);
-            
-                        if ($i % 2 == 1) {
-                            echo "<tr>";
-                        }  
+  <main>
+    <h1>Articles</h1>
+    <div class="article">
+      <script>
+        let valeur = 0;
+      </script>
 
-                        $nomProduit = $result[0]["nom"];
-                        $img = "SELECT image FROM produit WHERE id = $i";
-                        $res2 = ConnexionDB::getInstance()->querySelect($img);
-                        //print_r($res2[0]['image']);
-                        //$_SESSION[$nomProduit] = 0;
-                        $link = "img/".$res2[0]['image'];
-                        //echo $link;
-                        echo '<td> '; echo "<img src=$link>" ; echo '<p>'; echo $result[0]["nom"]; echo '</p>';
-                        echo '<br>';
-                        ?>
+      <table>
+        <tbody>
 
-                            <script> let nb = 0; </script> 
-                            <p id="valeur">Valeur : <span id="chiffre">0</span></p>
-                            <button onclick="incrementer()"> <img src="img/symboleMoins.png" id = "imageQuantiteMoins"> </button>
-                            <button onclick="incrementer()"> <img src="img/symbolePlus.png" id = "imageQuantitePlus"> </button> 
-                        </td>
-                        <?php
-                        echo '<style>
-                            #imageQuantiteMoins{
-                                width: 20px;
-                                height: 20px;
-                            }
-                            #imageQuantitePlus{
-                                width: 20px;
-                                height: 20px;
-                            }
-                            td {
-                                text-align: center;
-                                display: inline-block;
-                            }
+          <?php
+          ConnexionDB::getInstance();
+          $sql = "SELECT id FROM produit";
+          $result = ConnexionDB::getInstance()->querySelect($sql);
+          $nbProduct = count($result);
 
-                            button {
-                                border: none;
-                                background-color: white;
-                            }
+          for ($i = 1; $i <= $nbProduct; $i++) {
+            $nom = "SELECT nom FROM produit WHERE id = $i";
+            $result = ConnexionDB::getInstance()->querySelect($nom);
 
-                            '
-                            ;
-            
-                        if($i % 2 == 0){
-                            echo "</tr>";
-                        }
-                    }
+            if ($i % 2 == 1) {
+              echo "<tr>";
+            }
 
-                    
-?>
-                <tr>
-                    <td>article1</td>
-                    <td>article2</td>
-                </tr>
-            </tbody>
-        </table>
-        </div>
+            $nomProduit = $result[0]["nom"];
+            $_SESSION[$nomProduit] = 0;
+            $img = "SELECT image FROM produit WHERE id = $i";
+            $res2 = ConnexionDB::getInstance()->querySelect($img);
+            //print_r($res2[0]['image']);
+            $_SESSION[$nomProduit] = 0;
+            $link = "img/" . $res2[0]['image'];
+            //echo $link;
+            echo '<td> ';
+            echo "<img src=$link>";
+            echo '<h1>';
+            echo $result[0]["nom"];
+            echo '</h1>';
+            echo '<br>';
+            echo '<button onclick="decrementer(); decrementerphp($nomProduit);"> <img src="img/symboleMoins.png" id = "imageQuantiteMoins"></button>';
+            echo '<p id="valeur"> <span id="chiffre">0 </span></p>';
+            echo '<button onclick="augmenter(); augmenterphp($nomProduit);"> <img src="img/symbolePlus.png" id="imageQuantitePlus"></button>';
+            echo '</td>';
 
-        <script>
-  function incrementer() {
-    nb += 1;
-    // Mise à jour de la valeur dans le document
-    document.getElementById('chiffre').innerHTML = nb;
-  }
-</script>
-    </main>
-    
-    <footer>
-        <p>Développé par <a href="mailto:alexis.rosset@etu.univ-cotedazur.fr">Alexis Rosset</a> et <a href="mailto:leo.quelis@etu.univ-cotedazur.fr">Léo Quelis</a> - 2022-2023</p>
-    </footer>
+            if ($i % 2 == 0) {
+              echo "</tr>";
+            }
+          }
+          ?>
+
+
+
+          <script>
+            function augmenter() {
+              // Incrémentation de la valeur de la variable
+              //$_SESSION[$nomProduit] += 1;
+              valeur += 1;
+              // Mise à jour de la valeur dans le document
+              document.getElementById('chiffre').innerHTML = valeur;
+            }
+
+            function decrementer() {
+              // Incrémentation de la valeur de la variable
+              //$_SESSION['b'] += 1;
+              if (valeur - 1 >= 0) {
+                valeur -= 1;
+              }
+              // Mise à jour de la valeur dans le document
+              document.getElementById('chiffre').innerHTML = valeur;
+            }
+          </script>
+
+          <?php
+          function augmenterphp($nomProduit)
+          {
+            $_SESSION[$nomProduit] += 1;
+          }
+
+          function decrementerphp($nomProduit)
+          {
+            if ($_SESSION[$nomProduit] - 1 >= 0) {
+              $_SESSION[$nomProduit] -= 1;
+            }
+          }
+
+          ?>
+
+          <style>
+            #imageQuantiteMoins {
+              width: 20px;
+              height: 20px;
+            }
+
+            #imageQuantitePlus {
+              width: 20px;
+              height: 20px;
+            }
+
+            td {
+              text-align: center;
+              display: inline-block;
+            }
+
+            button {
+              border: none;
+              background-color: white;
+              display: inline-block;
+            }
+
+            p {
+              display: inline-block;
+            }
+          </style>
+        </tbody>
+
+  </main>
 </body>
+
 </html>
