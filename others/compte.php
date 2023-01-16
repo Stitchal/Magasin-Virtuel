2c3a6c90cf29dd42df429b73bfe56824af5de239
Leo Quelis <leo.quelis@etu.unice.fr>
2023-01-16 14:42:23 +0100

    fix bug

--- others/compte.php
+++ others/compte.php
@@ -1,33 +1,38 @@
 <?php
-    session_start();
-    $GLOBALS["page"] = "compte.php";
+session_start();
+$GLOBALS["page"] = "compte.php";
 
-    if (checkAdmin($_SESSION['nom']) ){
-        require_once(__DIR__.'/../includes/menu-admin.php');
-        require_once(__DIR__ . '/../libs/database.php');
-    }
 
-    if(empty($_SESSION['nom']) || empty($_SESSION['prenom']) || empty($_SESSION['email'])){
-        header('Location: index.php');
-        exit;
-    }
+if (empty($_SESSION['nom']) || empty($_SESSION['prenom']) || empty($_SESSION['email'])) {
+    header('Location: index.php');
+    exit;
+}
 
-    require_once(__DIR__.'/../includes/nav.php');
-    require_once(__DIR__ . '/../libs/database-functions.php');
+require_once(__DIR__ . '/../includes/nav.php');
+require_once(__DIR__ . '/../libs/database-functions.php');
+
+if (checkAdmin($_SESSION['nom'])) {
+    require_once(__DIR__ . '/../includes/menu-admin.php');
+    require_once(__DIR__ . '/../libs/database.php');
+}
+
+
 ?>
 
 <!DOCTYPE html>
 <html LANG="fr">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/responsive.css">
     <title>Informations du compte</title>
 </head>
+
 <body>
     <main>
-            <h1>Vue d'ensemble du compte</h1>
-            <div class="divCompte">
+        <h1>Vue d'ensemble du compte</h1>
+        <div class="divCompte">
             <table>
                 <tbody>
                     <tr>
@@ -40,19 +45,19 @@
                     </tr>
                     <tr>
                         <td> Statut </td>
-                        <td><?php if(checkAdmin($_SESSION['nom'])) : echo "admin" ?> <?php else : echo "pas admin"?> <?php endif; ?> </td>
+                        <td><?php if (checkAdmin($_SESSION['nom'])) : echo "admin" ?> <?php else : echo "pas admin" ?> <?php endif; ?> </td>
                     </tr>
                 </tbody>
             </table>
-            </div>
-            <?php
-            if(checkClientCommande($_SESSION["nom"])){
+        </div>
+        <?php
+        if (checkClientCommande($_SESSION["nom"])) {
             echo '<div>';
             echo '<a href="../client/client-factures.php" id="bouton" title="Cliquez ici passer la commande">Voir les factures</a>';
             echo '</div>';
-            }
-            ?>
+        }
+        ?>
     </main>
 </body>
-</html>
 
+</html>
